c0cca29f635ac85a7fdb407bec1f2ffff0a9cad3
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-06-28 19:00:46 +0530

    Product components changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,28 +10,76 @@ const Products = () => {
     const getProducts = async () => {
       setLoading(true);
       const res = await fetch('https://fakestoreapi.com/products');
-      if(componentMounted){
-          setData(await res.clone().json());
-          setFilter(await res.json());
-          setLoading(false);
-          console.log(filter)
+      if (componentMounted) {
+        setData(await res.clone().json());
+        setFilter(await res.json());
+        setLoading(false);
+        console.log(filter);
       }
       return () => {
-          componentMounted = false;
-      }
+        componentMounted = false;
+      };
     };
     getProducts();
   }, []);
 
-  return <div>
-     <div className="container my-5 py-5">
-          <div className="row">
-               <div className="col-12 mb-5">
-                    <h1 className='display-6 fw-bolder text-center'>Latest Products</h1>
-                    <hr /> 
-               </div>
+  const Loading = () => {
+    return <>Loading...</>;
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pd-5">
+          <button className="btn btn-outline-dark me-2">All</button>
+          <button className="btn btn-outline-dark me-2">Men's Clothing</button>
+          <button className="btn btn-outline-dark me-2">
+            Women's Clothing
+          </button>
+          <button className="btn btn-outline-dark me-2">Jewelery</button>
+          <button className="btn btn-outline-dark me-2">Electronics</button>
+        </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className='col-md-3 mb-4'>
+                <div class="card h-100 text-center p-4" key={product.id}>
+                  <img
+                    src={product.image}
+                    class="card-img-top"
+                    alt={product.title}
+                    height='200px'
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title mb-0">{product.title.substring(0,12)}</h5>
+                    <p class="card-text lead fw-bold">${product.price}</p>
+                    <a href="#" class="btn btn-outline-dark">
+                      Buy Now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+            <hr />
           </div>
-     </div>
-  </div>;
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Products;
