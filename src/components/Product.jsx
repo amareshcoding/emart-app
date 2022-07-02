464f8cfe4ba801ec20b8b4d0f927ccb2a74b506a
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-07-02 19:07:30 +0530

    Individual products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function Product() {
   const { id } = useParams();
@@ -18,23 +20,68 @@ export default function Product() {
     };
     getProduct();
   }, []);
-     
+
   const Loading = () => {
-     return <>Loading.....</>
-  }
+    return (
+      <>
+        <div className="col-md-6">
+          <Skeleton height={400} />
+        </div>
+        <div className="col-md-6" style={{ lineHeight: 2 }}>
+          <Skeleton height={50} width={300} />
+          <Skeleton height={75} />
+          <Skeleton height={25} width={150} />
+          <Skeleton height={50} />
+          <Skeleton height={150} />
+          <div style={{display:'flex', flexDirection:'horizontal'}}>
+            <Skeleton height={50} width={100} />
+            <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+          </div>
+        </div>
+      </>
+    );
+  };
 
   const ShowProduct = () => {
-     return(
-          <>
-          </>
-     )
-  }
+    return (
+      <>
+        <div className="col-md-6 mt-4">
+          <img
+            src={product.image}
+            alt={product.title}
+            height="400px"
+            width="400px"
+          />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="text-uppercase text-black-50">{product.category}</h4>
+          <h1 className="display-5"> {product.title}</h1>
+          <p className="lead fw-bolder">
+            Rating {product.rating && product.rate}
+            <i className="fa fa-star"></i>
+          </p>
+          <h3 className="display-6 fw-bold my-4">₹{product.price}</h3>
+          <p className="lead">{product.description}</p>
+          <button className="btn btn-outline-dark px-4 py-2">
+            {' '}
+            Add to Cart
+          </button>
+          <Link className="btn btn-dark ms-2 px-3 py-2" to="/">
+            {' '}
+            Go to Cart
+          </Link>
+        </div>
+      </>
+    );
+  };
 
-  return <div>
-     <div className="container">
-          <div className="row">
-               {loading? <Loading/> : <ShowProduct/>}
-          </div>
-     </div>
-  </div>;
+  return (
+    <div>
+      <div className="container py-5">
+        <div className="row py-4">
+          {loading ? <Loading /> : <ShowProduct />}
+        </div>
+      </div>
+    </div>
+  );
 }
